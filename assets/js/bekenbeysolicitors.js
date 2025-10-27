6279c45a4a72f5b5bba026117cde6085f65f285f
work wiseup <workwiseup@gmail.com>
2025-10-27 14:49:12 +0300

    upp

--- assets/js/bekenbeysolicitors.js
+++ assets/js/bekenbeysolicitors.js
@@ -82,43 +82,43 @@
   }
 
   // custom coursor
-  if ($(".custom-cursor").length) {
-    var cursor = document.querySelector(".custom-cursor__cursor");
-    var cursorinner = document.querySelector(".custom-cursor__cursor-two");
-    var a = document.querySelectorAll("a");
+  //if ($(".custom-cursor").length) {
+  //  var cursor = document.querySelector(".custom-cursor__cursor");
+  //  var cursorinner = document.querySelector(".custom-cursor__cursor-two");
+  //  var a = document.querySelectorAll("a");
 
-    document.addEventListener("mousemove", function (e) {
-      var x = e.clientX;
-      var y = e.clientY;
-      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
-    });
+  //  document.addEventListener("mousemove", function (e) {
+  //    var x = e.clientX;
+  //    var y = e.clientY;
+  //    cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+  //  });
 
-    document.addEventListener("mousemove", function (e) {
-      var x = e.clientX;
-      var y = e.clientY;
-      cursorinner.style.left = x + "px";
-      cursorinner.style.top = y + "px";
-    });
+  //  document.addEventListener("mousemove", function (e) {
+  //    var x = e.clientX;
+  //    var y = e.clientY;
+  //    cursorinner.style.left = x + "px";
+  //    cursorinner.style.top = y + "px";
+  //  });
 
-    document.addEventListener("mousedown", function () {
-      cursor.classList.add("click");
-      cursorinner.classList.add("custom-cursor__innerhover");
-    });
+  //  document.addEventListener("mousedown", function () {
+  //    cursor.classList.add("click");
+  //    cursorinner.classList.add("custom-cursor__innerhover");
+  //  });
 
-    document.addEventListener("mouseup", function () {
-      cursor.classList.remove("click");
-      cursorinner.classList.remove("custom-cursor__innerhover");
-    });
+  //  document.addEventListener("mouseup", function () {
+  //    cursor.classList.remove("click");
+  //    cursorinner.classList.remove("custom-cursor__innerhover");
+  //  });
 
-    a.forEach((item) => {
-      item.addEventListener("mouseover", () => {
-        cursor.classList.add("custom-cursor__hover");
-      });
-      item.addEventListener("mouseleave", () => {
-        cursor.classList.remove("custom-cursor__hover");
-      });
-    });
-  }
+  //  a.forEach((item) => {
+  //    item.addEventListener("mouseover", () => {
+  //      cursor.classList.add("custom-cursor__hover");
+  //    });
+  //    item.addEventListener("mouseleave", () => {
+  //      cursor.classList.remove("custom-cursor__hover");
+  //    });
+  //  });
+  //}
 
   if ($(".contact-form-validated").length) {
     $(".contact-form-validated").validate({
@@ -339,15 +339,15 @@
     });
   }
 
-  if ($(".wow").length) {
-    var wow = new WOW({
-      boxClass: "wow", // animated element css class (default is wow)
-      animateClass: "animated", // animation css class (default is animated)
-      mobile: true, // trigger animations on mobile devices (default is true)
-      live: true, // act on asynchronously loaded content (default is true)
-    });
-    wow.init();
-  }
+  //if ($(".wow").length) {
+  //  var wow = new WOW({
+  //    boxClass: "wow", // animated element css class (default is wow)
+  //    animateClass: "animated", // animation css class (default is animated)
+  //    mobile: true, // trigger animations on mobile devices (default is true)
+  //    live: true, // act on asynchronously loaded content (default is true)
+  //  });
+  //  wow.init();
+  //}
 
   if ($("#donate-amount__predefined").length) {
     let donateInput = $("#donate-amount");
